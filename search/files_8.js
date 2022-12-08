c2c689ef603cc28193dece2d697cd76d97409181
ShuraFairy <ShuraFairy@users.noreply.github.com>
2022-12-08 09:05:56 +0000

    deploy: 736c103f2d92822bbd9382dbd6d44e5f05eda6ea

---
+++ search/files_8.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['main_2ecpp_0',['main.cpp',['../main_8cpp.html',1,'']]],
+  ['main_2ecpp_2eo_2ed_1',['main.cpp.o.d',['../main_8cpp_8o_8d.html',1,'']]],
+  ['matchers_2emd_2',['matchers.md',['../matchers_8md.html',1,'']]],
+  ['matrix_2eh_3',['matrix.h',['../matrix_8h.html',1,'']]],
+  ['mocking_2emd_4',['mocking.md',['../mocking_8md.html',1,'']]]
+];
---
+++ search/pages_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['otus_5f06_2ehomework_0',['OTUS_06.homework',['../md__r_e_a_d_m_e.html',1,'']]]
+];
