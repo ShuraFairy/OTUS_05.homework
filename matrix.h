41475fde654e29daa021eb705bf7d71974fc09af
ShuraFairy <petrovai_net@mail.ru>
2022-12-08 11:59:23 +0300

    Changed matrix.h

--- matrix.h
+++ matrix.h
@@ -11,33 +11,37 @@ class Matrix
 	class MatrixElement
 	{
 	public:
-		MatrixElement(data_storage& data, size_t row, size_t col) : data(data), row(row), col(col)
+		MatrixElement(data_storage& data, size_t row, size_t column) : data(data), row(row), column(column)
 		{ };
 
-		operator T() {
-			return data.count({ row, col }) > 0 ? data[{row, col}] : default_value;
+		operator T() 
+		{
+			return data.count({ row, column }) > 0 ? data[{row, column}] : default_value;
 		}
-		operator const T() const {
-			return data.count({ row, col }) > 0 ? data[{row, col}] : default_value;
+		operator const T() const 
+		{
+			return data.count({ row, column }) > 0 ? data[{row, column}] : default_value;
 		}
-		MatrixElement& operator=(T element) {
+		MatrixElement& operator=(const T & element) 
+		{
 			if (element != default_value)
-				data[{row, col}] = element;
+				data[{row, column}] = element;
 			else
-				data.erase({ row,col });
+				data.erase({ row,column });
 			return *this;
 		};
 	private:
 		data_storage& data;
-		size_t row, col;
+		size_t row, column;
 	};
 
 	class MatrixRow
 	{
 	public:
 		MatrixRow(data_storage& data, size_t row) : data(data), row(row) {};
-		MatrixElement operator[](size_t columnindex) {
-			return MatrixElement(data, row, columnindex);
+		MatrixElement operator[](size_t column_index) 
+		{
+			return MatrixElement(data, row, column_index);
 		};
 	private:
 		data_storage& data;
@@ -45,34 +49,41 @@ class Matrix
 	};
 
 public:
-	class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+	class iterator : public std::iterator<std::bidirectional_iterator_tag, T> 
+	{
 	public:
 		iterator(typename data_storage::iterator iter) : iter(iter) {};
 		bool operator==(iterator& rhs) { return iter == rhs.iter; }
 		bool operator!=(iterator& rhs) { return iter != rhs.iter; }
 		iterator& operator--() { --iter; return *this; }
 		iterator& operator++() { ++iter; return *this; }
+
 		std::tuple<size_t, size_t, T> operator*() 
 		{ 
-			return { std::get<0>(iter->first), std::get<1>(iter->first), iter->second }; 
+			return { std::get<0>(iter->first), std::get<1>(iter->first), iter->second }; 				
 		}
 	protected:
 		typename data_storage::iterator iter;
 	};
 
-	class const_iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+	class const_iterator : public std::iterator<std::bidirectional_iterator_tag, T> 
+	{
 	public:
-		const_iterator(typename data_storage::iterator iter) : iter(iter) {};
-		bool operator==(iterator& rhs) { return iter == rhs.iter; }
-		bool operator!=(iterator& rhs) { return iter != rhs.iter; }
-		const iterator& operator--() const { --iter; return *this; }
-		const iterator& operator++() const { ++iter; return *this; }
-		std::tuple<size_t, size_t, T> operator*() { return { std::get<0>(iter->first), std::get<1>(iter->first), iter->second }; }
+		const_iterator(typename data_storage::const_iterator iter) : iter(iter) {};
+		bool operator==(const_iterator& rhs) const { return iter == rhs.iter; }
+		bool operator!=(const_iterator& rhs) const { return iter != rhs.iter; }
+		const const_iterator& operator--() const { --iter; return *this; }
+		const const_iterator& operator++() const { ++iter; return *this; }
+		std::tuple<size_t, size_t, T> operator*() 
+		{ 
+			return { std::get<0>(iter->first), std::get<1>(iter->first), iter->second 
+		}; 
+	}
 	protected:
-		typename data_storage::iterator iter;
+		typename data_storage::const_iterator iter;
 	};
 
-	size_t size() { return data.size(); };
+	size_t size() const { return data.size(); };
 
 	MatrixRow operator[](size_t index) 
 	{
@@ -80,6 +91,9 @@ public:
 	};
 	Matrix::iterator begin() { return data.begin(); };
 	Matrix::iterator end() { return data.end(); };
+
+	Matrix::iterator ñbegin() const{ return data.cbegin(); };
+	Matrix::iterator ñend() const { return data.cend(); };
 private:
 	data_storage data;
 };
